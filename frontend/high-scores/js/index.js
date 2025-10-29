297c9428381627b0d1182062564398d1e3e7d4b6
joemcdonough15 <joemcdonoughmusic@gmail.com>
2025-10-29 02:48:00 -0400

    render high scores to high-scores/index.html if there are any

--- frontend/high-scores/js/index.js
+++ frontend/high-scores/js/index.js
@@ -1,11 +1,38 @@
-// import { getHighScores } from "./api/highScoresApi";
-
-// const highScores = await getHighScores();
-
-import HighScores from "./highScoresApi.js";
+import HighScores from "./api/highScoresApi.js";
 
 const highScoresObj = new HighScores();
-
 const highScores = await highScoresObj.getHighScores();
 
-console.log("high scores: ", highScores);
+if (highScores.length > 0) {
+  const highScoresContainer = document.getElementById("high-scores-container");
+  const highScoresTable = document.createElement("table");
+  const highScoresTableHeader = document.createElement("thead");
+  const highScoresTableBody = document.createElement("tbody");
+  const highScoresTableHeaderRow = document.createElement("tr");
+  const highScoresTablePlayerNameHeader = document.createElement("td");
+  const highScoresTablePlayerScoreHeader = document.createElement("td");
+  highScoresTablePlayerNameHeader.innerText = "Player Name";
+  highScoresTablePlayerScoreHeader.innerText = "Score";
+  highScoresTableHeaderRow.append(
+    highScoresTablePlayerNameHeader,
+    highScoresTablePlayerScoreHeader
+  );
+  highScoresTableHeader.appendChild(highScoresTableHeaderRow);
+
+  highScores.forEach((scoreObj) => {
+    const highScoreBodyRow = document.createElement("tr");
+    const highScoreBodyRowPlayerName = document.createElement("td");
+    const highScoreBodyRowPlayerScore = document.createElement("td");
+    highScoreBodyRowPlayerName.innerText = scoreObj.name;
+    highScoreBodyRowPlayerScore.innerText = scoreObj.score;
+    highScoreBodyRow.append(
+      highScoreBodyRowPlayerName,
+      highScoreBodyRowPlayerScore
+    );
+
+    highScoresTableBody.appendChild(highScoreBodyRow);
+  });
+
+  highScoresTable.append(highScoresTableHeader, highScoresTableBody);
+  highScoresContainer.appendChild(highScoresTable);
+}
