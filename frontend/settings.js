aeeee16578b8604bee1946e53e05f0013fe6269b
joemcdonough15 <joemcdonoughmusic@gmail.com>
2025-10-31 16:19:18 -0400

    Begin building out instance methods for turning sound fx on and off

--- frontend/settings.js
+++ frontend/settings.js
@@ -1,11 +1,36 @@
 class Settings {
-  constructor() {
-    // sound fx - off or on
+  constructor(updateSettingsForm, savedSettings) {
+    this.updateSettingsForm = updateSettingsForm;
+    this.soundFx = savedSettings?.soundFx || "on";
     // music - off or on
     // music selection for in game play
     // color scheme
     // player controls
   }
+
+  turnSoundFxOn = () => {
+    this.soundFx = "on";
+  };
+
+  turnSoundFxOff = () => {
+    this.soundFx = "off";
+  };
+
+  prepareUpdateSettingsForm = () => {
+    this.updateSettingsForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      this.updateSettings();
+    });
+  };
+
+  updateSettings = () => {
+    const updateSoundFx = this.updateSettingsForm.elements.soundFx.value;
+    if (updateSoundFx === "on") {
+      this.turnSoundFxOn();
+    } else {
+      this.turnSoundFxOff();
+    }
+  };
 }
 
 export default Settings;
