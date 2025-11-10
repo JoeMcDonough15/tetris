ef19c8dbdf2f29ea4452d3fe6323177bb7ca8db8
joemcdonough15 <joemcdonoughmusic@gmail.com>
2025-11-10 03:34:30 -0500

    turn the three pages into functions.  Test for circular imports

---
+++ frontend/pages/highScores.js
@@ -0,0 +1,41 @@
+import HighScores from "../high-scores/js/api/highScoresApi.js";
+import {
+  createContainer,
+  createMenuButtons,
+  createHighScoresTable,
+  createCustomHeading,
+} from "../components/index.js";
+import {
+  mainMenuButtonsContainerObj,
+  highScoresMenuButtonObjs,
+  returnBody,
+} from "../utils/index.js";
+
+const highScoresPageBuilder = async () => {
+  const body = returnBody();
+  const highScoresContainer = createContainer("main", [
+    "high-scores-container",
+  ]);
+
+  body.prepend(
+    createCustomHeading("h1", "High Scores", ["main-heading"], "main-heading"),
+    highScoresContainer
+  );
+
+  const highScoresObj = new HighScores();
+  const highScores = await highScoresObj.getHighScores();
+
+  highScoresContainer.appendChild(
+    createMenuButtons(mainMenuButtonsContainerObj, highScoresMenuButtonObjs)
+  );
+
+  if (highScores.length) {
+    highScoresContainer.appendChild(createHighScoresTable(highScores));
+  } else {
+    highScoresContainer.appendChild(
+      createCustomHeading("h2", "No High Scores Yet", ["no-high-scores-header"])
+    );
+  }
+};
+
+export default highScoresPageBuilder;
